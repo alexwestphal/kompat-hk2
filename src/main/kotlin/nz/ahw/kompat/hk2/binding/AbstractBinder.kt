a59af7e5db2814715a0ef7c6a1c16076138c181d
Alex Westphal <alex@ahw.nz>
2017-12-28 15:13:28 +1300

    Added file copyright headers to source files

--- src/main/kotlin/nz/ahw/kompat/hk2/binding/AbstractBinder.kt
+++ src/main/kotlin/nz/ahw/kompat/hk2/binding/AbstractBinder.kt
@@ -1,3 +1,11 @@
+/*-----------------------------------------------------*\
+**                                                     **
+**        kompat-hk2: Kompatibility for HK2            **
+**        Kotlin Compatibility Project (Kompat)        **
+**        Copyright 2017-2018, Alex Westphal           **
+**        https://github.com/ahwnz/kompat-hk2          **
+**                                                     **
+\*-----------------------------------------------------*/
 package nz.ahw.kompat.hk2.binding
 
 import org.glassfish.hk2.api.Factory
